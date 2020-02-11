f3c998c53725c24fc8fc57070cdfc4945818e6be
masayuki_ozawa <masayuki.ozawa@live.jp>
2020-02-11 16:51:53 +0900

    no message

--- mssql2019-polybase/image-init.sh
+++ mssql2019-polybase/image-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /tmp
-wget https://packages.microsoft.com/ubuntu/16.04/mssql-server-2019/pool/main/m/mssql-server-polybase/mssql-server-polybase_15.0.2000.5-5_amd64.deb
-dpkg --install --force-depends /tmp/mssql-server-polybase_15.0.2000.5-5_amd64.deb
-rm /tmp/mssql-server-polybase_15.0.2000.5-5_amd64.deb
+wget https://packages.microsoft.com/ubuntu/16.04/mssql-server-2019/pool/main/m/mssql-server-polybase/mssql-server-polybase_15.0.4003.23-3_amd64.deb
+dpkg --install --force-depends /tmp/mssql-server-polybase_15.0.4003.23-3_amd64.deb
+rm /tmp/mssql-server-polybase_15.0.4003.23-3_amd64.deb
